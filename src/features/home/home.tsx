e85c0423568d2775c1385163e9a435fbccea2d0a
Yakraj Pariyar <59973173+yakraj@users.noreply.github.com>
2023-10-04 16:34:28 +0000

    pulled graphics inside of the content body

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -16,9 +16,10 @@ export const Home = () => {
   return (
     <>
       <div className="main-body b-center">
-        <div className="background-graphics">
+        <div className="graphical-content-body">
+          {/* background graphics starts here */}
           <svg
-            style={{ position: "absolute", top: 0, left: 0 }}
+            style={{ position: "absolute", top: "-10%", left: "-10%" }}
             xmlns="http://www.w3.org/2000/svg"
             width="25%"
             height="571"
@@ -39,51 +40,52 @@ export const Home = () => {
               width: "531px",
               borderRadius: "50%",
               background: "#7DAEA8",
-              bottom: "-50%",
-              right: "-10%",
+              bottom: "-40%",
+              right: "-20%",
             }}
           ></div>
-        </div>
-        <div className="content-body">
-          {/* content body has two sides left and right */}
-          {/* content body left side */}
-          <div className="content-body-left he-f">
-            {/* title starts here */}
-            <div className="branding-title">
-              {/* over graphics overlays starts here */}
-              <div className="firstYblur" />
-              <div className="seconddiff" />
-              <div className="thirdYbott" />
-              {/* over graphics overlays ends here */}
+          {/* background graphics ends here */}
+          <div className="content-body">
+            {/* content body has two sides left and right */}
+            {/* content body left side */}
+            <div className="content-body-left he-f">
+              {/* title starts here */}
+              <div className="branding-title">
+                {/* over graphics overlays starts here */}
+                <div className="firstYblur" />
+                <div className="seconddiff" />
+                <div className="thirdYbott" />
+                {/* over graphics overlays ends here */}
 
-              <div className="branding-name flex">
-                <h1 className="big-name">Yakraj</h1>
-                <p className="small-sirname">pariyar</p>
+                <div className="branding-name flex">
+                  <h1 className="big-name">Yakraj</h1>
+                  <p className="small-sirname">pariyar</p>
+                </div>
+                <p className="my-slogon">web developer</p>
               </div>
-              <p className="my-slogon">web developer</p>
-            </div>
-            {/* description */}
-            <p className="myself">
-              In internet I can make your existence and make you famous, A
-              business or a website that requires very precise and best front
-              end development, secure server, fast and interactive. I’m best on
-              my field.
-            </p>
+              {/* description */}
+              <p className="myself">
+                In internet I can make your existence and make you famous, A
+                business or a website that requires very precise and best front
+                end development, secure server, fast and interactive. I’m best
+                on my field.
+              </p>
 
-            <button className="hire-button c-p">HIRE ME</button>
-          </div>
-          {/* content body right side */}
-          <div className="content-body-right">
-            <div className="back-show-spi">
-              <img width="100%" alt="spinner" src={Spinner} />
+              <button className="hire-button c-p">HIRE ME</button>
             </div>
-            <div className="home-image-bio">
-              <img width="90%" className="myimage" alt="my image" src={Me} />
-              <div className="social-sites">
-                <img alt="twitter" className="c-p" src={twitter} />
-                <img alt="linkedin" className="c-p" src={linkedin} />
-                <img alt="github" className="c-p" src={github} />
-                <img alt="youtube" className="c-p" src={youtube} />
+            {/* content body right side */}
+            <div className="content-body-right">
+              <div className="back-show-spi">
+                <img width="100%" alt="spinner" src={Spinner} />
+              </div>
+              <div className="home-image-bio">
+                <img width="90%" className="myimage" alt="my image" src={Me} />
+                <div className="social-sites">
+                  <img alt="twitter" className="c-p" src={twitter} />
+                  <img alt="linkedin" className="c-p" src={linkedin} />
+                  <img alt="github" className="c-p" src={github} />
+                  <img alt="youtube" className="c-p" src={youtube} />
+                </div>
               </div>
             </div>
           </div>
